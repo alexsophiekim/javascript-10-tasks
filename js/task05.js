071d958bb61630f3e21b4432708a11616dad0be7
Sophie Kim <270010565@bun2-206-17.local>
2019-05-15 11:54:34 +1200

    done 5 out of 10

--- js/task05.js
+++ js/task05.js
@@ -1,3 +1,11 @@
 // Create an odd and even number checker
-var numberList=[];
-var number = parseInt(prompt(""))
+
+function checkOddEven(){
+  var numberToCheck = parseInt(prompt("Please enter a number"));
+  if((numberToCheck % 2 )==0){
+    //// that means even number
+    console.log(numberToCheck + " is an even number");
+  } else {
+    console.log(numberToCheck + " is an odd number");
+  }
+}
