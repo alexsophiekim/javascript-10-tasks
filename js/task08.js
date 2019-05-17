4b35273ffff16e0774a181a7845478505e1734f4
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-05-17 13:52:37 +1200

    solving number 8

--- js/task08.js
+++ js/task08.js
@@ -1,10 +1,14 @@
-// Create a script that asks for x amount of people. And then asks for their ages and finds the oldest and youngest age. As well as the average age
+// Create a script that asks for x amount of people.
+// And then asks for their ages and finds the oldest and youngest age. As well as the average age
 
-var x = paseInt(prompt("How many people?"));
+var x = parseInt(prompt("How many people?"));
+var oldest = 0;
 
 for (var i = 0; i < x.length; i++) {
   var age = parseInt(prompt("How old are you old?"));
   console.log(age);
-
+  if (oldest<age) {
+    oldest = age;
+  }
 }
- 
+console.log(age);
